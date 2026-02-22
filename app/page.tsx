4edac987886b243a46829429ad44dc7459206378
Gpedrozo <163456701+Gpedrozo@users.noreply.github.com>
2026-02-21 21:16:59 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
-
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "Arial"
+    }}>
+      <div style={{textAlign:"center"}}>
+        <h1>GPPIS</h1>
+        <p>Gestão Inteligente para Manutenções Industriais</p>
+      </div>
+    </main>
+  )
+}
